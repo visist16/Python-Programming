dab69d77b870acf43f02d0e0433094dbc9e89cc2
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-09-24 09:24:02 +0530

    Update Find largest among three numbers.py

--- Find largest among three numbers.py
+++ Find largest among three numbers.py
@@ -1,1 +1,13 @@
+#Find largest among three numbers
+N1 = int(input("Enter Number1: "))
+N2 = int(input("Enter Number2: "))
+N3 = int(input("Enter Number3: "))
 
+if N1 > N2 and N1 > N3:
+    print("The given Number1 is the largest among the given numbers.")
+elif N2 > N1 and N2 > N3:
+    print("The given Number2 is the largest among the given numbers.")
+elif N3 > N1 and N3 > N2:
+    print("The given Number3 is the largest among the given numbers.")   
+else:
+    print("The given number are equal.")
