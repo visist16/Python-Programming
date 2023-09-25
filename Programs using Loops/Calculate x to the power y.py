14ebc650c3b674282117a3622fbf91187770cebd
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-09-25 17:30:20 +0530

    Create Calculate x to the power y.py

---
+++ Programs using Loops/Calculate x to the power y.py
@@ -0,0 +1,9 @@
+# Input the base (x) and exponent (y)
+x = float(input("Enter the base (x): "))
+y = float(input("Enter the exponent (y): "))
+
+# Calculate x^y
+result = x ** y
+
+# Print the result
+print(f"{x} raised to the power of {y} is {result}.")
