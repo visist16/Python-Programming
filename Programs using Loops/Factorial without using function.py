5e2c6634c1d236a2e92df9b3a0ec956906d16765
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-09-25 17:19:57 +0530

    Update Factorial without using function.py

--- Programs using Loops/Factorial without using function.py
+++ Programs using Loops/Factorial without using function.py
@@ -1,1 +1,12 @@
+#Factorial without using function
+n = int(input("Enter a positive integer: "))
+factorial = 1
 
+if n<0:
+    print("Factorial is not defined for negative numbers.")
+elif n==0:
+    print("The factorial of 0 is 1.")
+else:
+    for i in range(1, n+1):
+        factorial *=i
+    print(f"The factorial of {n} is {factorial}.")    
