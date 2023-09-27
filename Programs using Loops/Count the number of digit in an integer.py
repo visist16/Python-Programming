c9d15dc0da05bef8b5c7b8bdd137640c442f531f
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-09-27 23:28:01 +0530

    Create Count the number of digit in an integer.py

---
+++ Programs using Loops/Count the number of digit in an integer.py
@@ -0,0 +1,5 @@
+#count of digits in a number
+number = int(input("Enter an integer: "))
+number_str = str(number)
+digit_count = len(number_str)
+print(f"The number of digits in {number} is: {digit_count}")
