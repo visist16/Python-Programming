c2db0a4210ada4df0d799bd0d6e122397ea41bcc
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-10-04 22:37:27 +0530

    Update Pascal trinagle.py

--- Programs using Loops/Pascal trinagle.py
+++ Programs using Loops/Pascal trinagle.py
@@ -1,4 +1,25 @@
 # Function to generate and print Pascal's Triangle
+# Print Pascal's Triangle in Python
+
+# input n
+n = 5
+
+for i in range(1, n+1):
+	for j in range(0, n-i+1):
+		print(' ', end='')
+
+	# first element is always 1
+	C = 1
+	for j in range(1, i+1):
+
+		# first value in a line is always 1
+		print(' ', C, sep='', end='')
+
+		# using Binomial Coefficient
+		C = C * (i - j) // j
+	print()
+
+#Other Method
 def print_pascals_triangle(num_rows):
     for i in range(num_rows):
         # Calculate the value of the current row
