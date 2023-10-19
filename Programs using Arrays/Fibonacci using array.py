2536e7002253b11247f18ddb44166751b19baf44
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-10-19 21:31:57 +0530

    Update Fibonacci using array.py

--- Programs using Arrays/Fibonacci using array.py
+++ Programs using Arrays/Fibonacci using array.py
@@ -1,1 +1,20 @@
+def generate_fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+
+    fibonacci = [0, 1]
+    while len(fibonacci) < n:
+        next_num = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_num)
+
+    return fibonacci
+
+# Example usage:
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
+fibonacci_sequence = generate_fibonacci(n)
+print(fibonacci_sequence)
 
