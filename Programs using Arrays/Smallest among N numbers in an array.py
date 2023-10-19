db1ae15a7b6b30c4266ddf058669690ac389f22a
Visist Tallam <57991461+visist16@users.noreply.github.com>
2023-10-19 21:33:30 +0530

    Update Smallest among N numbers in an array.py

--- Programs using Arrays/Smallest among N numbers in an array.py
+++ Programs using Arrays/Smallest among N numbers in an array.py
@@ -1,1 +1,16 @@
+def find_smallest_number(arr):
+    if not arr:
+        return None  # Handle the case of an empty array
+
+    smallest = arr[0]  # Initialize the smallest number with the first element
+    for number in arr:
+        if number < smallest:
+            smallest = number  # Update the smallest number if a smaller one is found
+
+    return smallest
+
+# Example usage:
+numbers = [34, 56, 12, 78, 90, 45]
+smallest_number = find_smallest_number(numbers)
+print("The smallest number is:", smallest_number)
 
